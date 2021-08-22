172fb9096e544bc2445a353c7a8d29040d078d63
cristianologrado <cristianologrado@yahoo.com>
2021-08-22 17:05:23 -0300

    Ajustes de formatacao / trata o caso em que o arquivo de links ja existe

--- Patentes.py
+++ Patentes.py
@@ -1,12 +1,12 @@
 
-#import libraries
+# import libraries
 import os
 import requests
 import urllib.request
 import pandas as pd
 
 
-#extrai a lista de anos para os quais ha pantentes, a partir da lista de heads nivel 3
+# extrai a lista de anos para os quais ha pantentes, a partir da lista de heads nivel 3
 def capturar_anos(heads_param):
     anos = []
     for head in heads_param:
@@ -30,7 +30,7 @@ def contar_patentes(anos_param, links_arquivos_param):
 
 def baixararquivos(diretorio_param, links_param, totalpatentes_param):
     count = 0
-    #loop para todos os links
+    # loop para todos os links
     for link in links_param:
         name = str(link.split('/')[-1])
         name = f'{diretorio_param}/{name}'
@@ -41,23 +41,27 @@ def baixararquivos(diretorio_param, links_param, totalpatentes_param):
             with open(name, 'wb') as handler:
                 handler.write(r.content)
         else:
-            print('nome :',name,' numero :', count, 'total :', totalpatentes_param,' existente')
+            print('nome :', name, ' numero :', count, 'total :', totalpatentes_param, ' existente')
 
 
 def capturardetalhes(links_param, arquivo_param):
-    listaarquivos = pd.DataFrame(columns=['ano', 'link', 'arquivo', 'tamanho'])
-    cont = 0
+    # se o arquivo de dados ainda não existe, gera os dados e cria o arquivo
+    if not os.path.exists(arquivo_param):
+        listaarquivos = pd.DataFrame(columns=['ano', 'link', 'arquivo', 'tamanho'])
+        cont = 0
+        for link in links_param:
+            aux = link.split('/')
+            listaarquivos.loc[cont, 'ano'] = int(aux[5])
+            listaarquivos.loc[cont, 'link'] = link
+            listaarquivos.loc[cont, 'arquivo'] = str(aux[6])
+            with urllib.request.urlopen(link) as handler:
+                listaarquivos.loc[cont, 'tamanho'] = int(handler.getheader('Content-Length'))
+                cont = cont + 1
+                print('contagem de links lidos : ', cont)
+        listaarquivos.to_csv(arquivo_param, sep=';')
+    # se o arquivo ja existe, faz a leitura e retorna o dataframe lido
+    else:
+        print('Arquivo já existe na pasta / arquivo existente retorna como resultado')
+        listaarquivos = pd.read_csv(arquivo_param, sep=';')
 
-    for link in links_param:
-        aux = link.split('/')
-        listaarquivos.loc[cont, 'ano'] = int(aux[5])
-        listaarquivos.loc[cont, 'link'] = l
-        listaarquivos.loc[cont, 'arquivo'] = str(aux[6])
-        with urllib.request.urlopen(link) as handler:
-            listaarquivos.loc[cont, 'tamanho'] = int(handler.getheader('Content-Length'))
-        cont = cont + 1
-        print('contagem ', cont)
-
-    listaarquivos.to_csv(arquivo_param,sep=';')
     return listaarquivos
-
--- main.py
+++ main.py
@@ -12,44 +12,38 @@
 
 
 # Importacao de Bibliotecas
-
 import os
-import requests
 import urllib.request
-from bs4 import BeautifulSoup as BS
-import pandas as pd
+from bs4 import BeautifulSoup
 import Patentes
 
 if __name__ == '__main__':
-
     url_base = 'http://storage.googleapis.com/'
     url_in = 'https://www.google.com/googlebooks/uspto-patents-grants-text.html'
     diretorio_arquivos = 'data'
 
 # leitura do htmls da paginas
     response = urllib.request.urlopen(url_in)
-    html_in = BS(response)
+    html_in = BeautifulSoup(response)
 
     anos = Patentes.capturar_anos(html_in.find_all('h3'))
 
 # importando os links
-links1 = html_in.find_all('a', href=True)
+    links1 = html_in.find_all('a', href=True)
 
 #filtrando links para os arquivos zip
-links2 = [l.get('href') for l in links1 if 'http://storage.googleapis.com/patents/grant_full_text' in l.get('href')]
-
-contagempatentes = Patentes.contar_patentes(anos,links2)
+    links2 = [l.get('href') for l in links1 if 'http://storage.googleapis.com/patents/grant_full_text' in l.get('href')]
+    contagempatentes = Patentes.contar_patentes(anos, links2)
 
 # salvando o total de patentes de cada ano
-contagempatentes.to_csv('contagem_patentes.csv',sep=';')
+    contagempatentes.to_csv('contagem_patentes.csv', sep=';')
 
 # grafico com total de patentes por ano
-#contagempatentes.plot(kind='bar',figsize=(15,6),xlabel='ano',ylabel='quantidade patantes no ano')
+    contagempatentes.plot(kind='bar',figsize=(15,6),xlabel='ano',ylabel='quantidade patantes no ano')
 
 #criar pasta para salvar arquivos de dados das patentes
-if not os.path.exists(diretorio_arquivos):
-    os.mkdir(diretorio_arquivos)
+    if not os.path.exists(diretorio_arquivos):
+        os.mkdir(diretorio_arquivos)
 
-Patentes.baixararquivos(diretorio_arquivos, links2[0:25], contagempatentes['quantidade'].sum())
-
-detalhes_por_ano = Patentes.capturardetalhes(links2,'detalhes_por_ano.csv')
+    Patentes.baixararquivos(diretorio_arquivos, links2[0:30], contagempatentes['quantidade'].sum())
+    detalhes_por_ano = Patentes.capturardetalhes(links2, 'detalhes_por_ano.csv')
